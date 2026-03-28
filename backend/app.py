4a0ea0e6c12cebb3ad57514ccc1e567277d479ca
mikiyas97 <mikiyashulualem@gmail.com>
2026-03-28 18:28:11 +0300

    adding some flask backend

---
+++ backend/app.py
@@ -0,0 +1,67 @@
+from flask import Flask, redirect, render_template, request
+import google.generativeai as genai
+import sqlite3
+
+
+
+
+def get_schema():
+    db = sqlite3.connect("movies.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT name, sql FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    schema = "Database schema:\n"
+    for table_name, create_sql in tables:
+        schema += create_sql + "\n"
+    db.close()
+    return schema
+
+schema = get_schema()
+
+def configure_ai(api_key):
+    genai.configure(api_key=api_key)
+    return genai
+model = configure_ai("AIzaSyC2ouZoXQEyzGGYS7CaEOTLnaqIG2y792c")
+system_prompt = f"""You are an expert SQL generator. Use ONLY the database schema provided.
+
+here is the database schema:\n
+{schema}
+
+
+Rules:
+- Return raw SQL only. No markdown, explanations, or comments.
+- Do NOT invent tables or columns that are not in the schema.
+- Fix typos and messy input in user questions automatically.
+- If a question mentions numbers, ranges, or patterns, use the schema column types to generate correct SQL.
+- Use JOINs if necessary, based on the schema relationships (primary/foreign keys).
+- Use COUNT, SUM, AVG when user asks for totals or averages.
+- Assume all columns and table names are exactly as provided in the schema.
+"""
+model_instance = model.GenerativeModel(model_name="gemini-2.5-flash", system_instruction=system_prompt)
+
+
+
+
+
+app = Flask(__name__)
+@app.route("/", methods=["GET"])
+def main():
+    return render_template("index.html")
+
+@app.route("/ask", methods=["POST", "GET"])
+def ask():
+    if request.method == "POST":
+        db = sqlite3.connect("movies.db")
+        cursor = db.cursor()
+
+        question = request.form.get("question")
+        response = model_instance.generate_content(question)
+
+        query = response.text
+        cursor.execute(query)
+        results = cursor.fetchall()
+        db.close()
+
+        return render_template("index.html", question=question, answer=results, query=query)
+    else:
+        return redirect("/")
---
+++ backend/static/script.js
@@ -0,0 +1,11 @@
+let messageInput = document.getElementById("message-input");
+let chatBox = document.getElementById("chat-box");
+let fileInput = document.getElementById("file-input");
+let submitBtn = document.getElementById("submit-btn");
+
+async function getData() {
+  let response = await fetch("http://127.0.0.1:5000/ask")
+  let data = await response.json()
+  console.log(data)
+}
+
